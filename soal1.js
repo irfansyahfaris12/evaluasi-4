3ea128a0274fcd986ef90096b9642c45b6c727b7
irfansyahfaris12 <52726995+irfansyahfaris12@users.noreply.github.com>
2019-08-10 14:50:09 +0700

    Update soal1.js

--- soal1.js
+++ soal1.js
@@ -1,6 +1,6 @@
 var n = 9
-for (i = 0; i < n; i++){
-    for (h = 0; h < n; h++){
+for (i = 0; i <= n; i++){
+    for (h = 0; h <= n; h++){
       if (h >= 8 - i) {
         document.write((n - i) * 2 + " ")
       }else{
