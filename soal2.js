046bbd4f8e37ea842cac3bb88c2d8dcbc22a44da
irfansyahfaris12 <52726995+irfansyahfaris12@users.noreply.github.com>
2019-08-10 14:47:06 +0700

    Update soal2.js

--- soal2.js
+++ soal2.js
@@ -2,9 +2,9 @@
   for (i = 0; i <= 8; i++){
     for (h = 0; h <= 8; h++){
       if (h == i) {
-        document.write(h * 2 - 1)
+        document.write(h * 2 + 1)
       }else if(i == 8 - h){
-        document.write(h * 2 - 2)
+        document.write(h * 2)
       }else if(h > i && i < 8 - h){
         document.write('A')
       }else if(h < i && i > 8 - h){
